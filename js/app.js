5c5d9fbd44725a82f45306f21284d9c02d08eacf
bojanS94 <eastcodewestcode94@gmail.com>
2021-01-03 11:36:06 +0100

    Finished landers v3

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$('.scrollBtn').click(function () {
+    $('html, body').animate({
+        scrollTop: $('.newsletter').offset().top + 'px'
+    }, 'slow');
+    $('#input').focus();
+});
+$('#subBtn').click(function () {
+    $('#input').focus();
+    let inputVal = $('#input').val();
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (inputVal === '') {
+        $('.error').hide();
+        $('.errorEmail').show();
+        $('.success').hide();
+        return;
+    }
+    else if (!regex.test(inputVal)) {
+        $('.error').show();
+        $('.errorEmail').hide();
+        $('.success').hide();
+    }
+    else {
+        $('.error').hide();
+        $('.errorEmail').hide();
+        $('.success').show();
+    }
+});
